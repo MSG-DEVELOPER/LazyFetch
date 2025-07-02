65bafd4f52f92987bde8e98d55706a4c832a9731
Joker <puebloseko@gmail.com>
2025-07-02 20:04:42 +0200

    Navbar done

--- FRONT/src/pages/HomePage/HomePage.tsx
+++ FRONT/src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@
 function HomePage() {
   return (
     <div>
-        HomePageeee
+       
        
 
     </div>
---
+++ FRONT/src/styles/colors.ts
@@ -0,0 +1,7 @@
+
+export const colors = {
+  primary: "#1F1F1F",
+  secondary: "#E5E5E5",
+  accent: "#007ACC",
+};
+
